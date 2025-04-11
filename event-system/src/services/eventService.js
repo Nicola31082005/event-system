02717aed391148d70de7b8f659f0f3c6d8a8ff43
Nicola31082005 <nikule512@gmail.com>
2025-04-11 14:06:16 +0300

    Add events and users services

---
+++ event-system/src/services/eventService.js
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client";
+import userService from "./userService";
+
+const prisma = new PrismaClient();
+
+export default {
+  getAllEvents: async () => {
+    return await prisma.event.findMany({
+      include: {
+        organizer: true,
+      },
+    });
+  },
+
+  getEventById: async (id) => {
+    return await prisma.event.findUnique({
+      where: { id },
+      include: {
+        organizer: true,
+      },
+    });
+  },
+
+  createEvent: async (data, clerkUserId) => {
+    // Get the database user associated with the Clerk user
+    const dbUser = await userService.getUserByClerkId(clerkUserId);
+    if (!dbUser) {
+      throw new Error("User not found");
+    }
+
+    return await prisma.event.create({
+      data: {
+        ...data,
+        organizerId: dbUser.id,
+      },
+      include: {
+        organizer: true,
+      },
+    });
+  },
+
+  deleteEvent: async (id, clerkUserId) => {
+    // Get the database user associated with the Clerk user
+    const dbUser = await userService.getUserByClerkId(clerkUserId);
+    if (!dbUser) {
+      throw new Error("User not found");
+    }
+
+    // Check if the user is the organizer of the event
+    const event = await prisma.event.findUnique({
+      where: { id },
+      select: { organizerId: true },
+    });
+
+    if (event?.organizerId !== dbUser.id) {
+      throw new Error("Unauthorized to delete this event");
+    }
+
+    return await prisma.event.delete({
+      where: { id },
+    });
+  },
+};
