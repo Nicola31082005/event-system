c3e97e76ec6da73c01c586e5ef3092f4a5ab52ed
Nicola31082005 <nikule512@gmail.com>
2025-04-16 15:49:20 +0300

    Add event routes error handling

--- event-system/src/app/api/events/[id]/route.js
+++ event-system/src/app/api/events/[id]/route.js
@@ -2,13 +2,21 @@ import eventService from "../../../../services/eventService";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-  const id = (await params).id;
-  const event = await eventService.getEventById(id);
-  return NextResponse.json(event);
+  try {
+    const id = (await params).id;
+    const event = await eventService.getEventById(id);
+    return NextResponse.json(event);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
 
 export async function DELETE(req, { params }) {
-  const id = (await params).id;
-  const event = await eventService.deleteEvent(id);
-  return NextResponse.json(event);
+  try {
+    const id = (await params).id;
+    const event = await eventService.deleteEvent(id);
+    return NextResponse.json(event);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
